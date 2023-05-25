c403f31a6800d06e334a8edbda005b429dc6554c
Alexandr <tribisalexandr@yandex.ru>
2023-05-26 00:03:23 +0300

    init: initial commit

---
+++ src/reducers/index.js
@@ -0,0 +1,30 @@
+const initialState = {
+  heroes: [], //heroes.json
+  heroesLoadingStatus: 'idle', //ничего не происходит
+  filters: [], //heroes.json
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'HEROES_FETCHING':
+      return {
+        ...state,
+        heroesLoadingStatus: 'loading',
+      };
+    case 'HEROES_FETCHED':
+      return {
+        ...state,
+        heroes: action.payload,
+        heroesLoadingStatus: 'idle',
+      };
+    case 'HEROES_FETCHING_ERROR':
+      return {
+        ...state,
+        heroesLoadingStatus: 'error',
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
